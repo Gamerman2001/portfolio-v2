f0edc0cd002e40c6a501ab6fb9f56a2820a679dc
Gamerman2001 <Tro12185@gmail.com>
2020-03-06 21:37:18 -0500

    editing config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `TroyGooden`,
+        short_name: `TroyGooden`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
