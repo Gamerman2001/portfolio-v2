1450a48a7c47e3e1f098dc530b7046dc9fd47974
Gamerman2001 <Tro12185@gmail.com>
2020-03-07 10:35:51 -0500

    adding content folder with sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,10 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
+    {console.log(props)}
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
